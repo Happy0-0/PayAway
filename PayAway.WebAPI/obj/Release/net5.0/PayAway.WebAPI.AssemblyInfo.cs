b505c971d39292f3280c8565559cb6d3b5d6e244
Gabriel Levit <Gabriel.Levit@FISGLOBAL.COM>
2020-11-02 14:02:18 -0500

    Test commit

--- PayAway.WebAPI/obj/Release/net5.0/PayAway.WebAPI.AssemblyInfo.cs
+++ PayAway.WebAPI/obj/Release/net5.0/PayAway.WebAPI.AssemblyInfo.cs
@@ -11,9 +11,9 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyMetadata("CompileTimestamp", "Monday, November 2, 2020 1:50:39 PM")]
+[assembly: System.Reflection.AssemblyMetadata("CompileTimestamp", "Monday, November 2, 2020 1:50:29 PM")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("PayAway.WebAPI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PayAway.WebAPI")]
