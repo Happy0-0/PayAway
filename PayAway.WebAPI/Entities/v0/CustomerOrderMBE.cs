bab5549f711b57c9601d1cb5b8e726eb7b886f3e
Tom Bruns <xtobr39@hotmail.com>
2020-11-28 10:14:55 -0500

    reorderd attributes in CustomerOrderMBE

--- PayAway.WebAPI/Entities/v0/CustomerOrderMBE.cs
+++ PayAway.WebAPI/Entities/v0/CustomerOrderMBE.cs
@@ -8,6 +8,37 @@ namespace PayAway.WebAPI.Entities.v0
 {
     public class CustomerOrderMBE
     {
+        [JsonIgnore]
+        public int OrderId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the order number
+        /// </summary>
+        /// <value>order number</value>
+        [JsonPropertyName("orderNumber")]
+        public string OrderNumber => this.OrderId.ToString("0000");
+
+        /// <summary>
+        /// Gets or sets order guid
+        /// </summary>
+        /// <value>merchantID</value>
+        [JsonPropertyName("orderGuid")]
+        public Guid OrderGuid { get; init; }
+
+        /// <summary>
+        /// Gets or sets the order date
+        /// </summary>
+        /// <value>order date</value>
+        [JsonPropertyName("orderDateTimeUTC")]
+        public DateTime OrderDateTimeUTC { get; set; }
+
+        /// <summary>
+        /// Gets or sets the order total
+        /// </summary>
+        /// <value>order total</value>
+        [JsonPropertyName("orderTotal")]
+        public decimal OrderTotal { get; set; }
+
         /// <summary>
         /// Gets or sets the merchant name
         /// </summary>
@@ -27,21 +58,7 @@ namespace PayAway.WebAPI.Entities.v0
         /// </summary>
         /// <value>The logo url</value>
         [JsonPropertyName("logoUrl")]
-        public Uri LogoUrl { get; set; }  
-        
-        /// <summary>
-        /// Gets or sets the order number
-        /// </summary>
-        /// <value>order number</value>
-        [JsonPropertyName("orderNumber")]
-        public string OrderNumber => this.OrderId.ToString("0000");
-
-        /// <summary>
-        /// Gets or sets the order total
-        /// </summary>
-        /// <value>order total</value>
-        [JsonPropertyName("orderTotal")]
-        public decimal OrderTotal { get; set; }
+        public Uri LogoUrl { get; set; }
 
         /// <summary>
         /// Gets or sets the orders customer name
@@ -56,23 +73,6 @@ namespace PayAway.WebAPI.Entities.v0
         [JsonPropertyName("phoneNumber")]
         public string CustomerPhoneNo { get; set; }
 
-        [JsonIgnore]
-        public int OrderId { get; set; }
-
-        /// <summary>
-        /// Gets or sets order guid
-        /// </summary>
-        /// <value>merchantID</value>
-        [JsonPropertyName("orderGuid")]
-        public Guid OrderGuid { get; init; }
-
-        /// <summary>
-        /// Gets or sets the order date
-        /// </summary>
-        /// <value>order date</value>
-        [JsonPropertyName("orderDateTimeUTC")]
-        public DateTime OrderDateTimeUTC { get; set; }
-
         /// <summary>
         /// Gets or sets a masked primary account number aka credit card number
         /// </summary>
@@ -83,12 +83,6 @@ namespace PayAway.WebAPI.Entities.v0
         /// Gets or set whether or not payment is available.
         /// </summary>
         [JsonPropertyName("isPaymentAvailable")]
-        public bool IsPaymentAvailable { get; set; }
-
-        
-
-        
-
-        
+        public bool IsPaymentAvailable { get; set; }   
     }
 }
