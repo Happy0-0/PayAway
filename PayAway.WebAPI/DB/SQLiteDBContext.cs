597d19a40378b9238122a475bfa5e2fc3a00944f
Tom Bruns <xtobr39@hotmail.com>
2020-12-03 06:38:32 -0500

    corrected text alignment

--- PayAway.WebAPI/DB/SQLiteDBContext.cs
+++ PayAway.WebAPI/DB/SQLiteDBContext.cs
@@ -72,17 +72,17 @@ namespace PayAway.WebAPI.DB
               .EnableSensitiveDataLogging();
         }
 
-    /// <summary>
-    /// Override this method to further configure the model that was discovered by convention from the entity types
-    /// exposed in <see cref="T:Microsoft.EntityFrameworkCore.DbSet`1" /> properties on your derived context. The resulting model may be cached
-    /// and re-used for subsequent instances of your derived context.
-    /// </summary>
-    /// <param name="modelBuilder">The builder being used to construct the model for this context. Databases (and other extensions) typically
-    /// define extension methods on this object that allow you to configure aspects of the model that are specific
-    /// to a given database.</param>
-    /// <remarks>If a model is explicitly set on the options for this context (via <see cref="M:Microsoft.EntityFrameworkCore.DbContextOptionsBuilder.UseModel(Microsoft.EntityFrameworkCore.Metadata.IModel)" />)
-    /// then this method will not be run.</remarks>
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+        /// <summary>
+        /// Override this method to further configure the model that was discovered by convention from the entity types
+        /// exposed in <see cref="T:Microsoft.EntityFrameworkCore.DbSet`1" /> properties on your derived context. The resulting model may be cached
+        /// and re-used for subsequent instances of your derived context.
+        /// </summary>
+        /// <param name="modelBuilder">The builder being used to construct the model for this context. Databases (and other extensions) typically
+        /// define extension methods on this object that allow you to configure aspects of the model that are specific
+        /// to a given database.</param>
+        /// <remarks>If a model is explicitly set on the options for this context (via <see cref="M:Microsoft.EntityFrameworkCore.DbContextOptionsBuilder.UseModel(Microsoft.EntityFrameworkCore.Metadata.IModel)" />)
+        /// then this method will not be run.</remarks>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // By convention, non-composite primary keys of type short, int, long, or Guid are set up to have values generated for inserted entities,
             // if a value isn't provided by the application. 
